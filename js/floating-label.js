fc60d1bf16db9dbfafb3885b11b1dbb67ffc2a1f
Nail` Khalimov <nail980@mail.ru>
2018-11-27 15:40:45 +0300

    init

---
+++ js/floating-label.js
@@ -0,0 +1,24 @@
+$('.input-container').each(function () {
+    var label = $(this).find('label');
+    var textarea = $(this).find('textarea');
+    var input = $(this).find('input');
+    $(textarea).on('focusin', function () {
+        $(label).addClass('input-filled');
+    });
+    $(textarea).on('focusout', function () {
+        if ($(this).val() === '') {
+            $(label).removeClass('input-filled');
+        }
+    })
+    $(input).on('focusin', function () {
+        $(label).addClass('input-filled');
+    });
+    $(input).on('focusout', function () {
+        if ($(this).val() === '') {
+            $(label).removeClass('input-filled');
+        }
+    })
+
+
+
+})
